f43d51c4f200fe8e4a8ee60ec5d3e691cf2da174
Jyno <jinho9613@naver.com>
2021-07-14 23:26:59 +0900

    add Clova STT

---
+++ src/Clova_STT/ClovaSpeechClientObject.py
@@ -0,0 +1,113 @@
+import requests
+import json
+
+import pathlib
+
+class ClovaSpeechClient:
+    # Clova Speech invoke URL
+    invoke_url = ''
+    # Clova Speech secret key
+    secret = ''
+
+    def __init__(self):
+        file_location_folder = str(pathlib.Path(__file__).parent.resolve())
+        with open(file_location_folder + "/" + "private.json") as client_json:
+            client_config = json.load(client_json)
+        client_config = client_config["ClovaSpeech"]
+        self.invoke_url = client_config["Invoke_URL"]
+        self.secret = client_config["Secret_Key"]
+
+    def req_url(self, url, completion, callback=None, userdata=None, forbiddens=None, boostings=None, sttEnable=True,
+                wordAlignment=True, fullText=True, script='', diarization=None, keywordExtraction=None, groupByAudio=False):
+        request_body = {
+            'url': url,
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'sttEnable': sttEnable,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'script': script,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+            'keywordExtraction': keywordExtraction,
+            'groupByAudio': groupByAudio,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'Content-Type': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        return requests.post(headers=headers,
+                             url=self.invoke_url + '/recognizer/url',
+                             data=json.dumps(request_body).encode('UTF-8'))
+
+    def req_object_storage(self, data_key, completion, callback=None, userdata=None, forbiddens=None, boostings=None, sttEnable=True,
+                           wordAlignment=True, fullText=True, script='', diarization=None, keywordExtraction=None, groupByAudio=False):
+        request_body = {
+            'dataKey': data_key,
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'sttEnable': sttEnable,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'script': script,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+            'keywordExtraction': keywordExtraction,
+            'groupByAudio': groupByAudio,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'Content-Type': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        return requests.post(headers=headers,
+                             url=self.invoke_url + '/recognizer/object-storage',
+                             data=json.dumps(request_body).encode('UTF-8'))
+
+    def req_upload(self, file, completion, callback=None, userdata=None, forbiddens=None, boostings=None, sttEnable=True,
+                   wordAlignment=True, fullText=True, script='', diarization=None, keywordExtraction=None, groupByAudio=False):
+        request_body = {
+            'language': 'ko-KR',
+            'completion': completion,
+            'callback': callback,
+            'userdata': userdata,
+            'sttEnable': sttEnable,
+            'wordAlignment': wordAlignment,
+            'fullText': fullText,
+            'script': script,
+            'forbiddens': forbiddens,
+            'boostings': boostings,
+            'diarization': diarization,
+            'keywordExtraction': keywordExtraction,
+            'groupByAudio': groupByAudio,
+        }
+        headers = {
+            'Accept': 'application/json;UTF-8',
+            'X-CLOVASPEECH-API-KEY': self.secret
+        }
+        # print(json.dumps(request_body).encode('UTF-8'))
+        files = {
+            'media': open(file, 'rb'),
+            'params': (None, json.dumps(request_body).encode('UTF-8'), 'application/json')
+        }
+        response = requests.post(headers=headers, url=self.invoke_url + '/recognizer/upload', files=files)
+        return response
+
+    def speech_cognition(self, filename):
+        # res = ClovaSpeechClient().req_url(url='http://example.com/media.mp3', completion='sync')
+        # res = ClovaSpeechClient().req_object_storage(data_key='data/media.mp3', completion='sync')
+        res = ClovaSpeechClient().req_upload(file=filename, completion='sync')
+        result = json.loads(res.text)
+        return result["text"]
+
+if __name__ == "__main__":
+    filename = "data/03-16man.wav"
+    result = ClovaSpeechClient().speech_cognition(filename)
+    print(result)
--- src/main.py
+++ src/main.py
@@ -9,12 +9,13 @@ from pathlib import Path
 
 from werkzeug.utils import secure_filename
 
-from  KSD_STT import stt_run as stt
+from KSD_STT import stt_run as azure_stt
 
+
+from Clova_STT.ClovaSpeechClientObject import ClovaSpeechClient as clova_stt
 
 NAVER_OCR_URL = "312ab1aaaad04cb4907e2bdfb246bc67.apigw.ntruss.com/custom/v1/3870/40d9e6658a8a7c8b7d764aa349b635ea318d81480956aa066bccd98e5a61f074"
 NAVER_OCR_SECRET = "VXdEdFJDZ2lrWWdRZ2FmYUpWV25SWWVaT0VuZFNPU2Y="
-
 
 path = Path(os.path.realpath(__file__))
 UPLOAD_FOLDER = str(path.parent.parent) + '/audioFiles'
@@ -22,9 +23,11 @@ UPLOAD_FOLDER = str(path.parent.parent) + '/audioFiles'
 app = Flask(__name__)
 app.config['UPLOAD_FOLDER'] = UPLOAD_FOLDER
 
+
 @app.route('/', methods=['GET'])
 def indexHtml():
     return render_template('index.html')
+
 
 @app.route('/stt', methods=['GET'])
 def sttHtml():
@@ -33,7 +36,7 @@ def sttHtml():
 
 @app.route('/api/ocr-requests', methods=['POST'])
 def newOCRRequest():
-    print(request.data[:1000])
+    # print(request.data[:1000])
     result = requests.post('https://{}/infer'.format(NAVER_OCR_URL), headers={
         'X-OCR-SECRET': NAVER_OCR_SECRET
     }, files={
@@ -46,12 +49,11 @@ def newOCRRequest():
             'images': [{
                 'format': 'jpg',
                 'name': 'image',
-                'data' : base64.b64encode(data).decode("utf-8")
+                'data': base64.b64encode(request.data).decode("utf-8")
             }]
         })
     })
     return json.dumps(result.json(), ensure_ascii=False)
-
 
 
 @app.route('/api/ocr-requests/s3', methods=['POST'])
@@ -73,38 +75,66 @@ def newOCRRequestS3():
 
     payload = json.dumps(request_json).encode('UTF-8')
     headers = {
-    'X-OCR-SECRET': NAVER_OCR_SECRET,
-    'Content-Type': 'application/json'
+        'X-OCR-SECRET': NAVER_OCR_SECRET,
+        'Content-Type': 'application/json'
     }
 
-    return json.dumps(requests.request("POST", 'https://{}/infer'.format(NAVER_OCR_URL), headers=headers, data = payload).json(), ensure_ascii=False)
+    return json.dumps(
+        requests.request("POST", 'https://{}/infer'.format(NAVER_OCR_URL), headers=headers, data=payload).json(),
+        ensure_ascii=False)
 
+
+# @app.route('/api/stt', methods=['POST'])
+# def sttApiAzure():
+#     if 'file' not in request.files:
+#         flash('No file part')
+#         return redirect(request.url)
+#
+#     file = request.files['file']
+#
+#     if file.filename == '':
+#         flash('No selected file')
+#         return redirect(request.url)
+#
+#     file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
+#
+#     result_str = azure_stt.run(bool_mic=False, file_name=UPLOAD_FOLDER + "/" + file.filename)
+#
+#     os.remove(UPLOAD_FOLDER + "/" + file.filename)
+#
+#     result = {'result': "test_val"}
+#
+#     result['result'] = result_str
+#
+#     return jsonify(result)
 
 
 @app.route('/api/stt', methods=['POST'])
-def sttApi():
+def sttApiClova():
     if 'file' not in request.files:
-            flash('No file part')
-            return redirect(request.url)
+        flash('No file part')
+        return redirect(request.url)
 
     file = request.files['file']
 
     if file.filename == '':
-            flash('No selected file')
-            return redirect(request.url)
+        flash('No selected file')
+        return redirect(request.url)
 
     file.save(os.path.join(app.config['UPLOAD_FOLDER'], file.filename))
 
-    result_str = stt.run(bool_mic = False, file_name = UPLOAD_FOLDER + "/" + file.filename)
+    # result_str = azure_stt.run(bool_mic=False, file_name=UPLOAD_FOLDER + "/" + file.filename)
+    result_str = clova_stt().speech_cognition(UPLOAD_FOLDER + "/" + file.filename)
 
     os.remove(UPLOAD_FOLDER + "/" + file.filename)
 
-    result = {'result' : "test_val"}
+    result = {'result': "test_val"}
 
     result['result'] = result_str
-    
-    return jsonify(result)
 
+    print(result)
+
+    return jsonify(result)
 
 
 if __name__ == '__main__':
